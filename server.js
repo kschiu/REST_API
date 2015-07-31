a58d9dd6a429fc3ae0835e26c6a107e5fbc3f331
Kevin Chiu <kschiu@andrew.cmu.edu>
2015-07-30 21:50:13 -0400

    Cleaned up some documentation and made it more readable

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 // server.js
-
-// call the packages we need
+//==============================================================================
+// INCLUDE REQUIRED PACKAGES
+//==============================================================================
 var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+// configure app to use bodyParser() to
+// get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+//==============================================================================
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'Hello World API' });   
 });
@@ -26,6 +27,7 @@ router.get('/', function(req, res) {
 // all of our routes will be prefixed with /api
 app.use('/api', router);
 
+//=============================================================================
 // START THE SERVER
 // =============================================================================
 app.listen(port);
